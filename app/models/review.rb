74f15f1a8ce92dc72c9880731c49e255af70465b
Joel Tannas <jtannas@gmail.com>
2018-03-02 12:59:06 -0700

    add validatation to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  validates :rating,
+    presence: true,
+    numericality: true,
+    :inclusion => { :in => 1..5 }
+  validates :user_id, presence: true
+  validates :product_id, presence: true
 end
